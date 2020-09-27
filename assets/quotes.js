620d6bde3c1b20fc5d04b03f7bd1a5e592bbda78
basstimam <64816240+basstimam@users.noreply.github.com>
2020-09-28 00:08:59 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "imam", quotes: "Whatever happens, keep breathing" },
+    { author: "imam", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "imam", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "imam", quotes: "I love you." },
+    { author: "imam", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "imam", quotes: "I love you." },
+    { author: "imam", quotes: "I'm sorry for loving you." },
 ];

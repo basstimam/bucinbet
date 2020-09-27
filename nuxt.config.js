0c6c4bbfe4e468a8669a0cffb9bfae9dcbed644e
basstimam <64816240+basstimam@users.noreply.github.com>
2020-09-28 06:58:16 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,14 +20,14 @@ export default {
   head: {
     // titleTemplate: "%s - " + process.env.npm_package_name,
     // title: process.env.npm_package_name || "",
-    title: "I love you.",
+    title: "Heyyyyyy",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" },
       { hid: "description", name: "description", content: process.env.npm_package_description || "" }
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/x-icon", href: "/ss.ico" },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Lilita+One&family=Montserrat&family=Pacifico&display=swap" }
     ]
   },
